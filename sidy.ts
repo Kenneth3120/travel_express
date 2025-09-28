d89154d3946959dacbfc0ca816ee5f2a9a3bea06
Kenneth Rebello <kennethrebello253@gmail.com>
2025-09-28 11:42:31 +0530

    Create sidy.ts

---
+++ sidy.ts
@@ -0,0 +1,90 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+interface MenuItem {
+  icon: string;
+  svgIcon: string;
+  label: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  @Input() isSidebarCollapsed = false;
+  @Output() sidebarToggle = new EventEmitter<void>();
+
+  menuItems: MenuItem[] = [
+    {
+      icon: 'dashboard',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"/>
+      </svg>`,
+      label: 'Dashboard',
+      route: '/dashboard/main'
+    },
+    {
+      icon: 'tower',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M4 21V10L1 6l4-6h14l4 6-3 4v11H4zm2-2h12v-8H6v8zm12-10h2.5l-2-2.7L16 6v3zm-6-7H8l2-2h4l2 2h-2zm6 2v1.3l2.5 2.7H18V6zm-12 1.3V6H3.5l2.5 3.3z"/>
+      </svg>`,
+      label: 'Tower Instances',
+      route: '/dashboard/tower-instances'
+    },
+    {
+      icon: 'security',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"/>
+      </svg>`,
+      label: 'Credential Types',
+      route: '/dashboard/credential-types'
+    },
+    {
+      icon: 'settings',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5 3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97 0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1 0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.66Z"/>
+      </svg>`,
+      label: 'Execution Environments',
+      route: '/dashboard/execution-environments'
+    },
+    {
+      icon: 'analytics',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z"/>
+      </svg>`,
+      label: 'Statistics',
+      route: '/dashboard/statistics'
+    },
+    {
+      icon: 'history',
+      svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M13 3a9 9 0 0 0-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.954 8.954 0 0 0 13 21a9 9 0 0 0 0-18zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/>
+      </svg>`,
+      label: 'Audit Logs',
+      route: '/dashboard/audit-logs'
+    }
+  ];
+
+  toggleSidebar() {
+    this.sidebarToggle.emit();
+  }
+
+  onMenuItemClick(item: MenuItem) {
+    console.log('Menu item clicked:', item.label);
+  }
+}
